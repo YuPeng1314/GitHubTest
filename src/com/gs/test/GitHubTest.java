87b733101e750c8d3463affc3dd5d260ba9ca605
YuPeng1314 <1076365808@qq.com>
2017-09-20 15:28:16 +0800

    这是我的第一个GitHubTest项目，添加了GitHubTest类

---
+++ src/com/gs/test/GitHubTest.java
@@ -0,0 +1,10 @@
+package com.gs.test;
+
+/**
+ * Created by Administrator on 2017/9/20.
+ */
+public class GitHubTest {
+    public static void main(String...args){
+        System.out.println("Hello! This is my Project!");
+    }
+}
